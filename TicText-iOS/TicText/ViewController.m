47f7cc691466775081b0c4b26f39339f6b080087
Kev1nChen <chenyufei@live.com>
2015-01-27 18:31:23 -0600

    First commit from Xcode

---
+++ TicText-iOS/TicText/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TicText
+//
+//  Created by Kevin Yufei Chen on 1/27/15.
+//  Copyright (c) 2015 Kevin Yufei Chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TicText-iOS/TicText/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicText
+//
+//  Created by Kevin Yufei Chen on 1/27/15.
+//  Copyright (c) 2015 Kevin Yufei Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
