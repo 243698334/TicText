7c726e76efef852a1cb60c392b671be7c789c400
Kev1nChen <chenyufei@live.com>
2015-02-06 12:20:59 -0600

    Add Pods for Parse and Facebook SDK. Create TTConstants.h/.m, add keys for PFUser/PFObject

---
+++ TicText-iOS/TicText/TTConstants.h
@@ -0,0 +1,54 @@
+//
+//  TTConstants.h
+//  TicText
+//
+//  Created by Kevin Yufei Chen on 2/5/15.
+//  Copyright (c) 2015 Kevin Yufei Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - NSUserDefaults
+
+
+#pragma mark - NSNotification
+
+
+#pragma mark - PFUser Class
+// Field keys
+extern NSString * const kTTUserDisplayNameKey;
+extern NSString * const kTTUserFacebookIDKey;
+extern NSString * const kTTUserProfilePictureKey;
+extern NSString * const kTTUserProfilePictureSmallKey;
+extern NSString * const kTTUserTicTextFriendsKey;
+
+
+#pragma mark - PFObject Tic Class
+// Class key
+extern NSString * const kTTTicClassKey;
+
+// Field keys
+extern NSString * const kTTTicSenderKey;
+extern NSString * const kTTTicRecipientKey;
+extern NSString * const kTTTicTimeLimitKey;
+extern NSString * const kTTTicSendTimestampKey;
+extern NSString * const kTTTicReceiveTimestampKey;
+extern NSString * const kTTTicStatusKey;
+extern NSString * const kTTTicContentKey;
+
+
+#pragma mark - PFObject Activity Class
+// Class key
+extern NSString * const kTTActivityClassKey;
+
+// Type values
+extern NSString * const kTTActivityTypeSend;
+extern NSString * const kTTActivityTypeFetch;
+
+// Field keys
+extern NSString * const kTTActivityTypeKey;
+extern NSString * const kTTActivityFromUserKey;
+extern NSString * const kTTActivityToUserKey;
+extern NSString * const kTTActivityContentKey;
+extern NSString * const kTTActivityTicKey;
+
---
+++ TicText-iOS/TicText/TTConstants.m
@@ -0,0 +1,53 @@
+//
+//  TTConstants.m
+//  TicText
+//
+//  Created by Kevin Yufei Chen on 2/6/15.
+//  Copyright (c) 2015 Kevin Yufei Chen. All rights reserved.
+//
+
+#import "TTConstants.h"
+
+#pragma mark - NSUserDefaults
+
+
+#pragma mark - NSNotification
+
+
+#pragma mark - PFUser Class
+// Field keys
+NSString * const kTTUserDisplayNameKey = @"displayName";
+NSString * const kTTUserFacebookIDKey = @"facebookId";
+NSString * const kTTUserProfilePictureKey = @"profilePicture";
+NSString * const kTTUserProfilePictureSmallKey = @"profilePictureSmall";
+NSString * const kTTUserTicTextFriendsKey = @"ticTextFriends";
+
+
+#pragma mark - PFObject Tic Class
+// Class key
+NSString * const kTTTicClassKey = @"Tic";
+
+// Field keys
+NSString * const kTTTicSenderKey = @"sender";
+NSString * const kTTTicRecipientKey = @"recipient";
+NSString * const kTTTicTimeLimitKey = @"timeLimit";
+NSString * const kTTTicSendTimestampKey = @"sendTimestamp";
+NSString * const kTTTicReceiveTimestampKey = @"receiveTimestamp";
+NSString * const kTTTicStatusKey = @"status";
+NSString * const kTTTicContentKey = @"content";
+
+
+#pragma mark - PFObject Activity Class
+// Class key
+NSString * const kTTActivityClassKey = @"Activity";
+
+// Type values
+NSString * const kTTActivityTypeSend = @"send";
+NSString * const kTTActivityTypeFetch = @"fetch";
+
+// Field keys
+NSString * const kTTActivityTypeKey = @"type";
+NSString * const kTTActivityFromUserKey = @"fromUser";
+NSString * const kTTActivityToUserKey = @"toUser";
+NSString * const kTTActivityContentKey = @"content";
+NSString * const kTTActivityTicKey = @"tic";
