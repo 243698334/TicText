cdb9b412d93e0ab4191fb5e6187f47059cfbcd44
Kev1nChen <chenyufei@live.com>
2015-02-25 16:16:35 -0600

    validate session when app become active

--- TicText-iOS/TicText/AppDelegate.m
+++ TicText-iOS/TicText/AppDelegate.m
@@ -84,6 +84,9 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    if ([TTUser currentUser]) {
+        [[TTSession sharedSession] validateSessionInBackground];
+    }
     [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
