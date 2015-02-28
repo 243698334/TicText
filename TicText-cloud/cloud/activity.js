bb49bce39f50b77cca7950531909820c0fb110de
Kev1nChen <chenyufei@live.com>
2015-02-27 21:15:35 -0600

    Cloud code fix

--- TicText-cloud/cloud/activity.js
+++ TicText-cloud/cloud/activity.js
@@ -2,27 +2,30 @@
 Parse.Cloud.beforeSave("Activity", function(request, response) {
     Parse.Cloud.useMasterKey();
     if (request.user) {
-        request.object.set('fromUser', request.user);
+        console.log("FromUser uid = [" + request.object.get("fromUserId") + "], from get(fromUserId)");
+        request.object.set("fromUser", request.user);
     } else {
-        var fromUserId = request.object.get(fromUserId);
+        var fromUserId = request.object.get("fromUserId");
         var fromUserQuery = new Parse.Query(Parse.User);
         fromUserQuery.get(fromUserId, {
             success: function(object) {
-                request.object.set('fromUser', object);
+                request.object.set("fromUser", object);
             },
             error: function(object, error) {
                 response.error("Invalid fromUserId.");
             }
         });
     }
-    var toUserId = request.object.get(toUserId);
+    var toUserId = request.object.get("toUserId");
+    console.log("ToUser uid = [" + toUserId + "]");
     var toUserQuery = new Parse.Query(Parse.User);
     toUserQuery.get(toUserId, {
         success: function(object) {
-            request.object.set('toUser', object);
+            request.object.set("toUser", object);
             response.success();
         },
         error: function(object, error) {
+            console.error(error);
             response.error("Invalid toUserId.");
         }
     });
--- TicText-cloud/cloud/tic.js
+++ TicText-cloud/cloud/tic.js
@@ -2,27 +2,30 @@
 Parse.Cloud.beforeSave("Tic", function(request, response) {
     Parse.Cloud.useMasterKey();
     if (request.user) {
-    	request.object.set('sender', request.user);
+    	console.log("Sender uid = [" + request.object.get("senderUserId") + "]");
+    	request.object.set("sender", request.user);
     } else {
-    	var senderUserId = request.object.get(senderUserId);
+    	var senderUserId = request.object.get("senderUserId");
     	var senderQuery = new Parse.Query(Parse.User);
     	senderQuery.get(senderUserId, {
     		success: function(object) {
-            	request.object.set('sender', object);
+            	request.object.set("sender", object);
         	},
         	error: function(object, error) {
             	response.error("Invalid senderUserId.");
         	}
     	});
     }
-    var recipientUserId = request.object.get(recipientUserId);
+    var recipientUserId = request.object.get("recipientUserId");
+    console.log("Recipient uid = [" + recipientUserId + "]");
     var recipientQuery = new Parse.Query(Parse.User);
     recipientQuery.get(recipientUserId, {
         success: function(object) {
-            request.object.set('recipient', object);
+            request.object.set("recipient", object);
             response.success();
         },
         error: function(object, error) {
+        	console.error(error);
             response.error("Invalid recipientUserId.");
         }
     });
