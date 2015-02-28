a015ca6ae4bfe8968e264df77357339e20b3d2da
Kev1nChen <chenyufei@live.com>
2015-02-27 20:19:20 -0600

    Script for linking a user's objectId to a pointer (cloud code)

--- TicText-cloud/cloud/tic.js
+++ TicText-cloud/cloud/tic.js
@@ -1,7 +1,20 @@
 // Create pointers for sender and recipient in the cloud data store
 Parse.Cloud.beforeSave("Tic", function(request, response) {
     Parse.Cloud.useMasterKey();
-    request.object.set('sender', request.user);
+    if (request.user) {
+    	request.object.set('sender', request.user);
+    } else {
+    	var senderUserId = request.object.get(senderUserId);
+    	var senderQuery = new Parse.Query(Parse.User);
+    	senderQuery.get(senderUserId, {
+    		success: function(object) {
+            	request.object.set('sender', object);
+        	},
+        	error: function(object, error) {
+            	response.error("Invalid senderUserId.");
+        	}
+    	});
+    }
     var recipientUserId = request.object.get(recipientUserId);
     var recipientQuery = new Parse.Query(Parse.User);
     recipientQuery.get(recipientUserId, {
