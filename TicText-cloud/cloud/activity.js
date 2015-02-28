fe304ca2e6a21afef1adc75fb6b69edbee3dd979
Kev1nChen <chenyufei@live.com>
2015-02-27 19:55:24 -0600

    Fix typo

--- TicText-cloud/cloud/activity.js
+++ TicText-cloud/cloud/activity.js
@@ -1,1 +1,15 @@
-var activity = 0;
+Parse.Cloud.beforeSave("Activity", function(request, response) {
+    Parse.Cloud.useMasterKey();
+    request.object.set('fromUser', request.user);
+    var toUserId = request.object.get(toUserId);
+    var toUserQuery = new Parse.Query(Parse.User);
+    toUserQuery.get(toUserId, {
+        success: function(object) {
+            request.object.set('toUser', object);
+            response.success();
+        },
+        error: function(object, error) {
+            response.error("Invalid toUserId.");
+        }
+    });
+});
--- TicText-cloud/cloud/tic.js
+++ TicText-cloud/cloud/tic.js
@@ -1,1 +1,16 @@
-var tic = 0;
+// Create pointers for sender and recipient in the cloud data store
+Parse.Cloud.beforeSave("Tic", function(request, response) {
+    Parse.Cloud.useMasterKey();
+    request.object.set('sender', request.user);
+    var recipientUserId = request.object.get(recipientUserId);
+    var recipientQuery = new Parse.Query(Parse.User);
+    recipientQuery.get(recipientUserId, {
+        success: function(object) {
+            request.object.set('recipient', object);
+            response.success();
+        },
+        error: function(object, error) {
+            response.error("Invalid recipientUserId.");
+        }
+    });
+});
