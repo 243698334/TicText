f838cc0dfdcd84af1e39ae7e96f73e5fac04672d
tkatzenbaer <me@katzenbaer.net>
2015-04-19 17:38:46 -0500

    Add comments

--- TicText-iOS/TicText/TTMessagesToolbarItem.h
+++ TicText-iOS/TicText/TTMessagesToolbarItem.h
@@ -11,13 +11,25 @@
 @class TTMessagesToolbar;
 @interface TTMessagesToolbarItem : UIButton
 
-// 
+// Reference to the toolbar that is showing this toolbar item.
+// Automatically set by the toolbar when it's initialized containing an instance of this item.
 @property (nonatomic, weak) TTMessagesToolbar *toolbar;
 
+// Used to give this item more or less width when presented in the toolbar.
 - (CGFloat)widthMultiplier;
+
+// Displayed by the toolbar's owner when this item is selected.
 - (UIView *)contentView;
+
+// Called by the toolbar when this button is selected.
+// @Note This is not called multiple times if this item is currently selected.
 - (void)buttonOnSelect:(TTMessagesToolbar *)toolbar;
+
+// Called by the toolbar when this button is deselected.
+// @Note This will not be called if the newly selected item's |switchViewOnAction| returns NO.
 - (void)buttonOnDeselect:(TTMessagesToolbar *)toolbar;
+
+// Used by the toolbar and its owner to determine interactions with this item.
 - (BOOL)switchViewOnAction;
 
 @end
